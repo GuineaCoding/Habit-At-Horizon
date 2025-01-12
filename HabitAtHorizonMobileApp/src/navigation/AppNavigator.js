21712adc55b7c3b0c3630e72073b51b277de086b
Andrian Barbulat <andrian.barbulat@gmail.com>
2025-01-12 21:45:40 +0000

    Organize authentication pages into 'auth' folder

--- HabitAtHorizonMobileApp/src/navigation/AppNavigator.js
+++ HabitAtHorizonMobileApp/src/navigation/AppNavigator.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignUpScreen';
-import PasswordResetScreen from '../screens/PasswordResetScreen';
+import LoginScreen from '../screens/user-auth/LoginScreen';
+import SignupScreen from '../screens/user-auth/SignUpScreen';
+import PasswordResetScreen from '../screens/user-auth/PasswordResetScreen';
 import AboutScreen from '../screens/AboutScren'
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="SignUp" component={SignupScreen} />
