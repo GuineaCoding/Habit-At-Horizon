810779c79bef957df2cfa0a5d3b2fc4177f4de77
Andrian Barbulat <andrian.barbulat@gmail.com>
2025-01-13 22:04:34 +0000

    Added Firebase Login functionality

--- HabitAtHorizonMobileApp/src/screens/user-auth/LoginScreen.js
+++ HabitAtHorizonMobileApp/src/screens/user-auth/LoginScreen.js
@@ -1,69 +1,60 @@
-// src/screens/LoginScreen.js
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const focusedColor = '#FFC0CB'; 
-
-  const handleLogin = () => {
-    console.log('Login attempt with:', email, password);
-    navigation.navigate('WelcomeScreen'); 
-  };
-  const naviegateToPassowrdResetScreen = () => {
-    navigation.navigate('PasswordResetScreen'); 
+  const handleLogin = async () => {
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+      navigation.navigate('Home');
+    } catch (error) {
+      setError("Failed to sign in. Please check your credentials and try again.");
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Login to Habit-at-Horizon</Text>
+      {error ? <Text style={styles.error}>{error}</Text> : null}
       <TextInput
+        label="Email address"
         value={email}
         onChangeText={setEmail}
         mode="outlined"
         style={styles.input}
-        theme={{ colors: { text: 'white', placeholder: 'white', primary: focusedColor, underlineColor: 'transparent', background: '#FFFFFF' } }}
         placeholder="Enter your email"
         keyboardType="email-address"
       />
       <TextInput
+        label="Password"
         value={password}
         onChangeText={setPassword}
         mode="outlined"
         style={styles.input}
         secureTextEntry
-        theme={{ colors: { text: 'white', placeholder: 'white', primary: focusedColor, underlineColor: 'transparent', background: '#FFFFFF' } }}
         placeholder="Enter your password"
       />
       <Button
         mode="contained"
         onPress={handleLogin}
-        buttonColor="#6D9773"
         style={styles.button}
       >
         Login
       </Button>
       <Button
         mode="contained"
-        onPress={() => {
-          setEmail('');
-          setPassword('');
-          naviegateToPassowrdResetScreen()
-        }}
-        buttonColor="#B46617"
+        onPress={() => navigation.navigate('PasswordResetScreen')}
         style={styles.button}
       >
         Reset Password
       </Button>
-
-      <Text style={styles.text}>
-        Hey, if you're here, you've already made the first step towards a better you.
-      </Text>
     </View>
   );
 };
@@ -76,24 +67,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#0C3B2E',
   },
   header: {
-    fontSize: 24,
     color: '#FFFFFF',
     marginBottom: 20,
     textAlign: 'center',
+    fontSize: 24,
   },
   input: {
-    backgroundColor: '#FFFFFF', 
     marginBottom: 10,
-    color: 'white', 
+    backgroundColor: '#FFFFFF',
   },
   button: {
     marginVertical: 10,
   },
-  text: {
-    marginTop: 30,
-    color: '#FFFFFF',
-    fontSize: 14,
+  error: {
+    color: 'red',
     textAlign: 'center',
+    marginBottom: 10,
   }
 });
 
